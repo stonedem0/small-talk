c0c85f8a13f428a5e6ba029d40c827310215249e
Asya <anundam@gmail.com>
2021-10-04 18:49:26 +0100

    add onmessage event

--- client/chat.js
+++ client/chat.js
@@ -1,19 +1,25 @@
-let ws  = new WebSocket('ws://' + window.location.host + '/ws');
-
-
-// const form = document.getElementById('form');
-
+const ws = new WebSocket('ws://' + window.location.host + '/ws');
 
 const sendMessage = event => {
     const usersname = document.getElementById('username').value;
-    const messsage = document.getElementById('message').value
+    const message = document.getElementById('message').value
     ws.send(JSON.stringify({
         username: usersname,
-        message: messsage
+
+        message: message
     }))
     event.preventDefault();
 }
- 
+
+ws.onmessage = function (event) {
+    const chat = document.createElement("div");
+    const username = document.createTextNode(`${document.getElementById('username').value}: `);
+    const message = document.createTextNode(document.getElementById('message').value);
+    chat.appendChild(username);
+    chat.appendChild(message);
+    const currentDiv = document.getElementById("input_div");
+    document.body.insertBefore(chat, currentDiv.nextSibling)
+};
 
 const submit = document.getElementById('submit');
 submit.addEventListener('submit', sendMessage);
