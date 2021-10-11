98fcb6e89d587b1924b68b2368601948da5e4c02
Asya <anundam@gmail.com>
2021-10-11 19:03:24 +0100

    remove logs

--- client/chat.js
+++ client/chat.js
@@ -6,28 +6,23 @@ ws.onmessage = event => {
     displayMessages(event)
 };
 
-
-ws.onopen = async event => {
-
-    const data =  await fetch('./history')
+ws.onopen = async () => {
+    const data = await fetch('./history')
         .then(function (response) {
-            console.log('DATA', response);
             return response.text();
-
         }).then(function (payload) {
             const data = payload.trim().split('\n').map(JSON.parse)
             return data
- 
         });
-    // console.log('DATA', result);
+
     const history = document.createElement("div")
     const currentDiv = document.getElementById("input_div");
-    data.map( e => {
-        const p = document.createElement('p')   
-        const message = document.createTextNode(`${e.username} : ${ e.message} `)
+    data.map(e => {
+        const p = document.createElement('p')
+        const messages = document.createTextNode(`${e.username}: ${ e.message}`)
         document.body.insertBefore(history, currentDiv)
         history.appendChild(p);
-        history.appendChild(message);
+        history.appendChild(messages);
     })
 
 }
@@ -35,7 +30,7 @@ const sendMessage = event => {
     const usersname = document.getElementById('username').value;
     if (!usersname) {
         alert('nuh, tell us your username first')
-        return
+        // return
     }
     const message = document.getElementById('message').value
     ws.send(JSON.stringify({
@@ -51,7 +46,6 @@ const displayMessages = event => {
     const currentDiv = document.getElementById("input_div");
     const username = document.createTextNode(`${data.username}: `);
     const message = document.createTextNode(data.message);
-    // chat.setAttribute('style', 'height: 500px')
     document.body.insertBefore(chat, currentDiv)
     chat.appendChild(username);
     chat.appendChild(message);
