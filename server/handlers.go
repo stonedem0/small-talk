f3f4a658c3495b83eb7744b3acda3a7a388818fa
Asya <anundam@gmail.com>
2025-07-16 19:12:43 +0100

    add WithCORS

--- server/handlers.go
+++ server/handlers.go
@@ -33,17 +33,22 @@ func NewHandler(rdb *redis.Client, jwtSecret []byte) *Handler {
 	}
 }
 
-func (h *Handler) WithAuth(next http.HandlerFunc) http.HandlerFunc {
+func WithCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Handle preflight
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Content-Type", "application/json")
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		next(w, r)
+	}
+}
 
+func (h *Handler) WithAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		username, err := h.VerifyToken(r)
 		if err != nil {
 			http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
@@ -55,15 +60,7 @@ func (h *Handler) WithAuth(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func setHeaders(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	w.Header().Set("Content-Type", "application/json")
-}
-
 func (h *Handler) GetRoomsHandler(w http.ResponseWriter, r *http.Request) {
-	setHeaders(w)
 
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
@@ -82,7 +79,6 @@ func (h *Handler) GetRoomsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) SubscribeToRoomHandler(w http.ResponseWriter, r *http.Request) {
-	setHeaders(w)
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
@@ -112,8 +108,6 @@ func (h *Handler) SubscribeToRoomHandler(w http.ResponseWriter, r *http.Request)
 	w.Write([]byte("Subscribed successfully"))
 }
 func (h *Handler) GetChatHistoryHandler(w http.ResponseWriter, r *http.Request) {
-	setHeaders(w)
-
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
@@ -166,7 +160,6 @@ func (h *Handler) GetOnlineUsersHandler(w http.ResponseWriter, r *http.Request) 
 }
 
 func (h *Handler) GetRoomUsernamesHandler(w http.ResponseWriter, r *http.Request) {
-	setHeaders(w)
 
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
@@ -189,7 +182,6 @@ func (h *Handler) GetRoomUsernamesHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (h *Handler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	setHeaders(w)
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
@@ -248,9 +240,6 @@ func (h *Handler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) LoginHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("LoginHandler called!")
-	setHeaders(w)
-
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
