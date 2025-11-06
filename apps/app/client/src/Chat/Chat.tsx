16749e21b5728626117e2b0f68bcad3bf9c54d2f
Asya <anundam@gmail.com>
2025-11-06 18:17:30 +0000

    add room mane to ui

--- apps/app/client/src/Chat/Chat.tsx
+++ apps/app/client/src/Chat/Chat.tsx
@@ -487,6 +487,7 @@ const Chat = ({ username }: ChatProps) => {
           </div>
         </div>
         <div className="online-users-sidebar">
+          <h3 style={{ marginTop: 0, marginBottom: "8px" }}>{roomName}</h3>
           <h4>Online</h4>
             <ul>
               {onlineUsers
