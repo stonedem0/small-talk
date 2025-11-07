67b7b13f464b65b4acf54273de0a6a032688aaf8
Asya <anundam@gmail.com>
2025-11-07 21:14:14 +0000

    handling the histogram metric correctly

--- apps/app/server/metrics.go
+++ apps/app/server/metrics.go
@@ -12,6 +12,10 @@ type NodeStats struct {
 	CPUPercent    float64
 }
 
+func estimateCPUPercent() float64 {
+	return 0
+}
+
 func collectNodeStats() NodeStats {
 	samples := []metrics.Sample{
 		{Name: "/cpu/classes/gc/mark/assist:cpu-seconds"},
@@ -34,11 +38,19 @@ func collectNodeStats() NodeStats {
 		case "/memory/classes/heap/objects:bytes":
 			s.RSSMB = sm.Value.Uint64() / 1024 / 1024
 		case "/gc/pauses:seconds":
-			s.LastGCPauseMS = uint64(sm.Value.Float64() * 1000)
+			if hist := sm.Value.Float64Histogram(); hist != nil && len(hist.Counts) > 0 && len(hist.Buckets) > 1 {
+				for i := len(hist.Counts) - 1; i >= 0; i-- {
+					if hist.Counts[i] > 0 {
+						upper := hist.Buckets[i+1]
+						s.LastGCPauseMS = uint64(upper * 1000.0)
+						break
+					}
+				}
+			}
 		}
 	}
 
-	s.CPUPercent = estimateCPUPercent() // optional helper you can stub or leave 0
+	s.CPUPercent = estimateCPUPercent()
 	s.NumGC = uint32(samples[1].Value.Uint64())
 	return s
 }
