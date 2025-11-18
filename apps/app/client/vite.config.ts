dbbfc7b2952ef4bda26b7d677e2dd4acc1d82a25
Asya <anundam@gmail.com>
2025-11-18 20:03:39 +0000

    add ec to vite config

--- apps/app/client/vite.config.ts
+++ apps/app/client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: ["ec2-18-175-194-80.eu-west-2.compute.amazonaws.com"],
+  },
 })
